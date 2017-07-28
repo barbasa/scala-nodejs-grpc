17f3c17a2d7e34bf260a2616a3a9e71b371220ee
Fabio Ponciroli <fabio.ponciroli@net-a-porter.com>
2017-07-28 14:57:37 +0100

    Nodejs grpc client

---
+++ webclient/app.js
@@ -0,0 +1,21 @@
+let grpc = require('grpc');
+let services = require('./lib/proto/restaurants_grpc_pb');
+let messages = require('./lib/proto/restaurants_pb');
+
+const API_URL = '127.0.0.1';
+const API_PORT = '50051';
+
+console.log(`Connecting to API ${API_URL}:${API_PORT}`);
+
+let client = new services.SearchRestaurantServiceClient(`${API_URL}:${API_PORT}`, grpc.credentials.createInsecure());
+
+let protoRequest = new messages.SearchRestaurantRequest();
+
+protoRequest.setPostcode("w120je")
+
+client.searchRestaurant(protoRequest, function(response){
+    console.log(response)
+    // response.getRestaurantsList().map(function (r) {
+    //     console.log(r.toObject())
+    // });
+})
